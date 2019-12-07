c3571661ee838c0b56fa7398a3d942aaa4bf43b1
KaWaite <kyle.waite@outlook.com>
2019-12-07 11:34:03 +0900

    initial project version

---
+++ src/Components/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <div>
+      <h1>
+        Hello from above <i class="material-icons">3d_rotation</i>
+      </h1>
+      <p>This is the NavBar Component</p>
+    </div>
+  );
+}
